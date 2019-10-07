de0379da8e39c35543faa41fa15f632c4331ef34
Valerie Wiklund <vkwiklund@gmail.com>
2019-10-07 15:14:42 -0600

    added interfaces

--- Services/MenuItemService.cs
+++ Services/MenuItemService.cs
@@ -11,19 +11,19 @@ namespace Burgershack.Services
 
     public void Setup()
     {
-      Sandwich burger = new Sandwich("Happy Burger", 3.95, "Grilled hamburger patty on a sesame seed bun with lettuce, tomato, onion, ketchup and mustard");
-      Sandwich chburger = new Sandwich("Cheesy Burger", 4.95, "Grilled hamburger patty with 2 slices of American cheese on a sesame seed bun with lettuce, tomato, onion, ketchup and mustard");
+      Sandwich burger = new Sandwich("Happy Burger", "Grilled hamburger patty on a sesame seed bun with lettuce, tomato, onion, ketchup and mustard", 3.95, 1500, 30, 25, 15);
+      Sandwich chburger = new Sandwich("Cheesy Burger", "Grilled hamburger patty with 2 slices of American cheese on a sesame seed bun with lettuce, tomato, onion, ketchup and mustard", 4.95, 1500, 30, 25, 15);
 
-      Drink soda = new Drink("Soda", 1.50, "Your preferred flavor from our fountain drinks");
-      Drink shake = new Drink("Milkshake", 2.95, "Yummy, delicious ice cream drink");
+      Drink soda = new Drink("Soda", "Your preferred flavor from our fountain drinks", 1.50, 350, 0, 0, 15, true, .25);
+      Drink shake = new Drink("Milkshake", "Yummy, delicious ice cream drink", 2.95, 350, 0, 0, 15, false, .25);
 
-      Side fries = new Side("French Fries", 1.50, "Shoestring fries cooked to a crispy golden brown");
-      Side tots = new Side("Tater Tots", 1.95, "potato tots fried crispy");
+      Side fries = new Side("French Fries", "Shoestring fries cooked to a crispy golden brown", 1.50, 350, 30, 0, 25, .25);
+      Side tots = new Side("Tater Tots", "potato tots fried crispy", 1.95, 350, 30, 0, 25, .25);
 
       Items.AddRange(new MenuItem[] { burger, chburger, soda, shake, fries, tots });
     }
 
-    internal void GetMenu(int index)
+    internal void GetMenu(int index, string )
     {
       if (index < Items.Count && index > -1)
       {
@@ -44,7 +44,7 @@ namespace Burgershack.Services
       for (int i = 0; i < Items.Count; i++)
       {
         MenuItem item = Items[i];
-        Messages.Add($"{i + 1}: {item.Name} - {item.Price}");
+        Messages.Add($"{i + 1}: {item.Name} - {item.BasePrice}");
       }
       Messages.Add("Type a number to see details or Q to quit");
     }
